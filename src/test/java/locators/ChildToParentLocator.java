367d1ced5cc4a208f07b50329b8e0b1648733bc8
kunalchavan <kunal24chavan@gmail.com>
2025-07-15 01:15:48 +0530

    Happy Path Firefox and Edge

---
+++ src/test/java/locators/ChildToParentLocator.java
@@ -0,0 +1,22 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ChildToParentLocator {
+    @Test
+    public void locators() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/SeleniumDrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        // xpath
+        driver.findElement(By.xpath("(//header[@class='jumbotron text-center header_style']/div//following-sibling::button[text()='Practice']/parent::div/parent::header//div//following-sibling::button[1])[2]"));
+    }
+}
