4c21de410975e31d9b4f66c34d31963296d710c9
kunalchavan <kunal24chavan@gmail.com>
2025-07-16 01:24:28 +0530

    frame, Links, Actions

---
+++ src/test/java/frames/Frame.java
@@ -0,0 +1,29 @@
+package frames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Frame {
+
+    @Test
+    public void testFrames(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/SeleniumDrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://jqueryui.com/droppable/");
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+        // driver.switchTo().frame(0);
+        Actions action = new Actions(driver);
+        WebElement source = driver.findElement(By.cssSelector("body #draggable"));
+        WebElement target = driver.findElement(By.cssSelector("body #droppable"));
+        action.dragAndDrop(source,target).build().perform();
+        driver.close();
+    }
+}
---
+++ src/test/java/linkcount/Links.java
@@ -0,0 +1,22 @@
+package linkcount;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Links {
+    @Test
+    public void testLinkCount() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/SeleniumDrivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        int linkCount = driver.findElements(By.tagName("a")).size();
+        System.out.println("Link count is: " + linkCount);
+        driver.close();
+    }
+}
